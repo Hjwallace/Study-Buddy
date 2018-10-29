a0d44235778025ef7458e0f2c0a5ee04d3a1a687
AvidCuberGaming <jeshallcross@bsu.edu>
2018-10-29 09:41:38 -0400

    Web Player Implementation

--- .idea/src/edu/bsu/cs222/mainUI.java
+++ .idea/src/edu/bsu/cs222/mainUI.java
@@ -1,24 +1,21 @@
 package edu.bsu.cs222;
 
-
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -29,20 +26,10 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
 import java.awt.*;
-import java.awt.MenuBar;
-import java.awt.event.MouseEvent;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 import java.util.Optional;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.atomic.AtomicReference;
 
+@SuppressWarnings("deprecation")
 public class mainUI extends Application{
 
     @Override
@@ -51,18 +38,126 @@ public class mainUI extends Application{
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double screenHeight = screenSize.getHeight();
         double screenWidth = screenSize.getWidth();
-
+        String googleUserAgent = "Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1";
+        String chromeUserAgent = "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36";
+        String youtubeUserAgent = "Mozilla/5.0 (Linux; Android 4.4.4; One Build/KTU84L.H4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.135 Mobile Safari/537.36";
 
         primaryStage.setTitle("Study Buddy v1");
         VBox mainWindow = new VBox();
         HBox componentWindow = new HBox();
 
+
+
+        //User Side
+        VBox userSide = new VBox(10);
+        TextArea userText = new TextArea("User types here");
+        userText.setWrapText(true);
+        userSide.setAlignment(Pos.TOP_LEFT);
+        userSide.setPadding(new Insets(10,10,10,10));
+
+        //Music Box
+        TextArea musicPlayer = new TextArea("Music box will be here");
+        //---End Music Box---
+
+        //Conection
+        Connection connection = new Connection();
+        Label networkConnectionLabel = new Label();
+        //------------Color Loop for Connection Label------------
+        mainWindow.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                boolean connectionStatus = connection.findConnection();
+                if (connectionStatus){
+                    networkConnectionLabel.setText("Connection is Present");
+                    networkConnectionLabel.setTextFill(Color.color(0.0,1.0,0.0));
+                }
+                else {
+                    networkConnectionLabel.setText("Connection is not Present");
+                    networkConnectionLabel.setTextFill(Color.color(1.0,0.0,0.0));
+                }
+            }
+        });
+        //-----End Connection-------
+
+        userSide.getChildren().addAll(userText,musicPlayer,networkConnectionLabel);
+        //END USER SIDE
+
+        //Component Side
+        VBox componentSide = new VBox(10);
+        WebView component1 = new WebView();
+        WebEngine engine1 = component1.getEngine();
+        WebView component2 = new WebView();
+        WebEngine engine2 = component2.getEngine();
+        engine1.setJavaScriptEnabled(true);
+        engine2.setJavaScriptEnabled(true);
+        //engine1.setUserAgent("Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
+        //engine2.setUserAgent("Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
+        componentSide.getChildren().addAll(component1,component2);
+        componentSide.setAlignment(Pos.TOP_RIGHT);
+        componentSide.setPadding(new Insets(10,10,10,10));
+        //END COMP SIDE
+
+        //Line Seperator
+        Line seperator = LineBuilder.create().startX(screenWidth/2).startY(0).endX((screenWidth/2)+1).endY(screenHeight).fill(Color.BLACK).build();
+        //----------------------
+
+        userSide.setFillWidth(true);
+
         javafx.scene.control.MenuBar menuBar = new javafx.scene.control.MenuBar();
 
         Menu menuEdit = new Menu("Edit");
         Menu menuView = new Menu("View");
+
+        //Window One menu options-------------------------
         Menu menuWindowOne = new Menu("Window 1");
+        MenuItem youtubeButton1 = new MenuItem("Youtube");
+        youtubeButton1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                engine1.setUserAgent(youtubeUserAgent);
+                engine1.load("https://www.youtube.com");
+            }
+        });
+        MenuItem googleButton1 = new MenuItem("Google");
+        googleButton1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                engine1.setUserAgent(googleUserAgent);
+                engine1.load("https://www.google.com");
+            }
+        });
+        MenuItem bsuButton1 = new MenuItem("myBSU");
+        bsuButton1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                engine1.setUserAgent(googleUserAgent);
+                engine1.load("https://my.bsu.edu/");
+            }
+        });
+
+        menuWindowOne.getItems().addAll(youtubeButton1, googleButton1, bsuButton1);
+        //end of Window One menu options------------------
+
+        //Window Two menu options-------------------------
         Menu menuWindowTwo = new Menu("Window 2");
+        MenuItem youtubeButton2 = new MenuItem("Youtube");
+        youtubeButton2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                engine2.setUserAgent(youtubeUserAgent);
+                engine2.load("https://www.youtube.com");
+            }
+        });
+        MenuItem googleButton2 = new MenuItem("Google");
+        googleButton2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                engine2.setUserAgent(googleUserAgent);
+                engine2.load("https://www.google.com");
+            }
+        });
+        menuWindowTwo.getItems().addAll(youtubeButton2, googleButton2);
+        //end of Window Two menu options------------------
         Menu menuHelp = new Menu("Help");
 
         //File Options--------------------------
@@ -82,62 +177,8 @@ public class mainUI extends Application{
         menuFile.getItems().addAll(openFile,saveFile);
         //End File options------------------------
 
-
-        menuBar.getMenus().addAll(menuFile,menuEdit,menuView,menuWindowOne,menuWindowTwo,menuHelp);
-
-        //User Side
-        VBox userSide = new VBox(10);
-        TextArea userText = new TextArea("User types here");
-        userText.setWrapText(true);
-        userSide.setAlignment(Pos.TOP_LEFT);
-        userSide.setPadding(new Insets(10,10,10,10));
-
-        //Music Box
-        TextArea musicPlayer = new TextArea("Music box will be here");
-        //---End Music Box---
-
-        //Conection
-        Connection connection = new Connection();
-        boolean connectionStatus = connection.findConnection();
-        Label networkConnectionLabel = new Label();
-        //------------Color Loop for Connection Label------------
-        if (connectionStatus){
-            networkConnectionLabel.setText("Connection is Present");
-            networkConnectionLabel.setTextFill(Color.color(0.0,1.0,0.0));
-        }
-        else {
-            networkConnectionLabel.setText("Connection is not Present");
-            networkConnectionLabel.setTextFill(Color.color(1.0,0.0,0.0));
-        }
-        //-----End Connection-------
-
-        userSide.getChildren().addAll(userText,musicPlayer,networkConnectionLabel);
-        //END USER SIDE
-
-        //Component Side
-        VBox componentSide = new VBox(10);
-        WebView component1 = new WebView();
-        WebEngine engine1 = component1.getEngine();
-        WebView component2 = new WebView();
-        WebEngine engine2 = component2.getEngine();
-        engine1.setJavaScriptEnabled(true);
-        engine2.setJavaScriptEnabled(false);
-        engine1.setUserAgent("Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
-        engine2.setUserAgent("Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1");
-        engine1.load("https://www.google.com");
-        engine2.load("https://www.wikipedia.org");
-
-        componentSide.getChildren().addAll(component1,component2);
-        componentSide.setAlignment(Pos.TOP_RIGHT);
-        componentSide.setPadding(new Insets(10,10,10,10));
-        //END COMP SIDE
-
-        //Line Seperator
-        Line seperator = LineBuilder.create().startX(screenWidth/2).startY(0).endX((screenWidth/2)+1).endY(screenHeight).fill(Color.BLACK).build();
-        //----------------------
-
-        userSide.setFillWidth(true);
         componentWindow.getChildren().addAll(userSide,seperator,componentSide);
+        menuBar.getMenus().addAll(menuFile,menuEdit,menuView,menuWindowOne,menuWindowTwo,menuHelp);
         mainWindow.getChildren().addAll(menuBar,componentWindow);
         primaryStage.setScene(new Scene(mainWindow,screenWidth,screenHeight));
 
@@ -145,6 +186,7 @@ public class mainUI extends Application{
         String style = this.getClass().getResource("style.css").toExternalForm();
         mainWindow.getStylesheets().add(style);
         //CSS end
+
         mainWindow.heightProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
